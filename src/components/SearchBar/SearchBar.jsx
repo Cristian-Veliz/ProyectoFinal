d64094dfd251f8f3cdedb101d647b1ed76f8bf12
lautinieva4 <lautinieva4@gmail.com>
2023-09-18 11:57:35 -0300

    cambios

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import style from './SearchBar.module.css';
+import { useDispatch } from 'react-redux';
+import { getFurnituresByName, getAllFurnitures } from '../../components/redux/actions/Actions';
+
+export default function SearchBar({ setSearch }) {
+const [nameFurniture, setNameFurniture] = useState('');
+const dispatch = useDispatch();
+
+function handleChange(e) {
+    setNameFurniture(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (nameFurniture) {
+      dispatch(getFurnituresByName(nameFurniture));
+      setSearch(nameFurniture);
+      setNameFurniture('');
+    }
+  }
+  function handleClickFurnitures(e){
+    e.preventDefault();
+    dispatch(getAllFurnitures())
+    setSearch('');
+  }
+
+
+  return (
+    <form className={style.container} onSubmit={handleSubmit}>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        placeholder="Enter furniture"
+        value={nameFurniture}
+        onChange={handleChange}
+      />
+      <button type="submit">Search</button>
+      <button type="submit" onClick={handleClickFurnitures}>All</button>
+    </form>
+  );
+}
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,22 +1,34 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import CardContainer from '../../components/CardContainer/CardContainer';
 import { getAllFurnitures } from '../../components/redux/actions/Actions';
-
+import SearchBar from '../../components/SearchBar/SearchBar';
 
 function Home() {
-
   const dispatch = useDispatch();
   const allFurnitures = useSelector((state) => state.allFurnitures);
-
+  
   useEffect(() => {
     // Llamo a la acción que obtiene todas las furnitures
     dispatch(getAllFurnitures()); 
   }, [dispatch]);
 
+  const [searchFurniture , setSearchFurniture] = useState('');
+
+  const handleSearch = (furniture) => {
+    setSearchFurniture(furniture.toLowerCase());
+  };
+
+  // Filtrar los muebles que coincidan con la búsqueda
+  const filteredFurnitures = allFurnitures.filter((furniture) =>
+    furniture.name.toLowerCase().includes(searchFurniture)
+  );
+
   return (
     <div>
-      <CardContainer allFurnitures={allFurnitures} />
+      {/* Pasar filteredFurnitures al CardContainer en lugar de allFurnitures */}
+      <CardContainer allFurnitures={filteredFurnitures} />
+      <SearchBar setSearch={handleSearch} />
     </div>
   );
 }
