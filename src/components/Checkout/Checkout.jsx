4859704fc1892c5f9b15d1badcb6e150308958e0
Cristian-Veliz <cristianveliz6@gmail.com>
2023-09-28 20:41:46 -0300

    checkout

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -73,7 +73,7 @@ const Checkout = () => {
       email,
       direccion,
       nota,
-      ordenId:ordenIdAleatorio,
+      // ordenId:ordenIdAleatorio,
       orderDate:obtenerFechaActual()
     };
     
